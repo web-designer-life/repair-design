83e6aad9f47d02028e3389db8183d6938e55e5d4
web-designer-life <io57196657@gmail.com>
2020-03-14 23:32:15 +0300

    refact: main.js, index.html, style.css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
   var modal = document.querySelector('.modal');
   var input = document.querySelector('.modal input[type=text]');
@@ -24,5 +25,48 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (event.key === 'Escape'){
       modal.classList.remove('modal--visible');
     }
+  });
+});
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close'),
+      input = $('.modal input[type=text]'),
+      switchModal = function() {
+        modal.toggleClass('modal--visible'),
+        input.focus();   
+      };
+
+  modalBtn.on('click', switchModal);
+
+  closeBtn.on('click', function () {
+    modal.removeClass('modal--visible');
+  });
+
+  modal.keyup(function(event) {
+      if (event.key === 'Escape') {
+        modal.removeClass('modal--visible');
+      }
+  });
+  
+  modal.on('click', function (event) {
+    if (modal.has(event.target).length == 0) {
+      modal.toggleClass('modal--visible');
+    }
+  });
+ 
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+  $('.scrollup').fadeIn();
+  } else {
+  $('.scrollup').fadeOut();
+  }
+  });
+  
+  $('.scrollup').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
   });
 });
