74a4f2ae1f38c93eb1fab6dda0353f2526454c5c
web-designer-life <io57196657@gmail.com>
2020-03-26 11:56:06 +0300

    fix: main.js

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
   $('.scrollup').fadeOut();
   }
   });
-  
+   
   $('.scrollup').click(function(){
   $("html, body").animate({ scrollTop: 0 }, 1500);
   return false;
